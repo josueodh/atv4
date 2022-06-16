12fc732eba64695dcc67c13c1a587dc8184d9918
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-06-15 23:22:00 -0300

    Implementando passagem da informação da main activity para o juros simples

--- app/src/main/java/br/ufjf/dcc196/mltiplasactivities/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/mltiplasactivities/MainActivity.java
@@ -6,18 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editTextValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnSimples = findViewById(R.id.buttonSimples);
+        editTextValue = findViewById(R.id.editValor);
         btnSimples.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,SimplesActivity.class);
+                intent.putExtra("value",Integer.parseInt(editTextValue.getText().toString()));
                 startActivity(intent);
             }
         });
--- app/src/main/java/br/ufjf/dcc196/mltiplasactivities/SimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/mltiplasactivities/SimplesActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class SimplesActivity extends AppCompatActivity {
-
+    Integer value = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simples);
         Toast.makeText(getApplicationContext(), "ALo", Toast.LENGTH_LONG).show();
+        Bundle bundleExtra = getIntent().getExtras();
+        if(bundleExtra!= null){
+            value = bundleExtra.getInt("value");
+            System.out.println(value);
+        }
     }
 }
