7c1fe959ab793d42944a5bb4a1450d5ae2aadb70
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-06-17 20:08:43 -0300

    Refatorando simpleActivity layout e função do click

--- app/src/main/java/br/ufjf/dcc196/mltiplasactivities/SimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/mltiplasactivities/SimplesActivity.java
@@ -7,49 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SimplesActivity extends AppCompatActivity {
-    Integer value = 0;
-    Integer periodo = 0;
-    Integer taxa = 0;
-    Integer juros = 0;
+    private Double valorPresente;
+    private Double valorFinal;
+    Double taxaDeJuros;
+    Integer periodos;
+    private TextView textViewValorPresente;
+    private EditText editTextTaxaDeJuros;
+    private EditText editTextPeriodos;
+    private TextView textViewResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simples);
-        Button btnVoltar = findViewById(R.id.buttonVoltar);
-        Bundle bundleExtra = getIntent().getExtras();
+        textViewValorPresente = findViewById(R.id.textViewValorPresente);
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getDouble("value");
+        textViewValorPresente.setText("Valor : $" +valorPresente.toString());
 
-        if(bundleExtra!= null){
-            value = bundleExtra.getInt("value");
-        }
-
-        btnVoltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent resultado = new Intent();
-                resultado.putExtra("juros", juros.toString());
-                setResult(MainActivity.RESULT_SIMPLES,resultado);
-                finish();
-            }
-        });
+        editTextTaxaDeJuros = findViewById(R.id.editTaxaJuros);
+        editTextPeriodos = findViewById(R.id.editPeriodo);
+        textViewResultado = findViewById(R.id.textViewResultadoSimples);
     }
 
-    public void calcularSimples(View origem){
-        EditText editTextTaxa = findViewById(R.id.editTaxaJuros);
-        EditText editTextPeriodo = findViewById(R.id.editPeriodo);
-        try{
-            taxa = Integer.parseInt(editTextTaxa.getText().toString());
-        }catch(Exception e){
-            editTextTaxa.requestFocus();
-        }
-        try{
-            periodo = Integer.parseInt(editTextPeriodo.getText().toString());
-        }catch(Exception e){
-            editTextPeriodo.requestFocus();
-        }
-
-        juros = periodo * taxa * value;
+    public void calcularClick(View view ){
+        taxaDeJuros = Double.parseDouble(editTextTaxaDeJuros.getText().toString())/100.0;
+        periodos = Integer.parseInt(editTextPeriodos.getText().toString());
+        valorFinal = valorPresente*(1+taxaDeJuros*periodos);
+        textViewResultado.setText("Resultado: $" +valorFinal.toString());
     }
 }
