8f1fcf2c3289e9aad10054907c1d9d6a52032dc2
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-06-17 22:14:20 -0300

    Adicionando exibição da relação no main

--- app/src/main/java/br/ufjf/dcc196/mltiplasactivities/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/mltiplasactivities/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText editTextValue;
     private TextView textViewValorFinal;
+    private TextView textViewRelacao;
     public static final int RESULT_SIMPLES = 1;
     public static final int RESULT_COMPOSTO = 2;
     public static final int RESULT_RELACAO = 3;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewValorFinal = findViewById(R.id.textResultado);
+        textViewRelacao = findViewById(R.id.textResultadoRelacao);
         editTextValue = findViewById(R.id.editValor);
         launcher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -34,22 +36,27 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         Double valorFinal;
+                        Double valorFinalRelacao;
                         Bundle extras;
                         switch (result.getResultCode()){
                             case RESULT_SIMPLES:
                                 extras = result.getData().getExtras();
                                 valorFinal = extras.getDouble("resultado");
                                 textViewValorFinal.setText("Simples: R$"+valorFinal.toString());
+                                valorFinalRelacao = extras.getDouble("relacao");
+                                textViewRelacao.setText("Relação: "+valorFinalRelacao.toString());
                                 break;
                             case RESULT_COMPOSTO:
                                 extras = result.getData().getExtras();
                                 valorFinal = extras.getDouble("resultado");
                                 textViewValorFinal.setText("Compostos: R$"+valorFinal.toString());
+                                valorFinalRelacao = extras.getDouble("relacao");
+                                textViewRelacao.setText("Relação: "+valorFinalRelacao.toString());
                                 break;
                             case RESULT_RELACAO:
                                 extras = result.getData().getExtras();
-                                valorFinal = extras.getDouble("resultado");
-                                textViewValorFinal.setText("Compostos: R$"+valorFinal.toString());
+                                valorFinalRelacao = extras.getDouble("relacao");
+                                textViewRelacao.setText("Relação: "+valorFinalRelacao.toString());
                                 break;
                         }
 
