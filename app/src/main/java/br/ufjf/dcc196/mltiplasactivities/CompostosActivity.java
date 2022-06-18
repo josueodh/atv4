ed17d7b38960ebcd8ef4afc550d116b4e547bd36
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-06-17 21:36:19 -0300

    Finalizando implementação do Juros composto

--- app/src/main/java/br/ufjf/dcc196/mltiplasactivities/CompostosActivity.java
+++ app/src/main/java/br/ufjf/dcc196/mltiplasactivities/CompostosActivity.java
@@ -2,13 +2,50 @@ package br.ufjf.dcc196.mltiplasactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CompostosActivity extends AppCompatActivity {
 
+    private Double valorPresente;
+    private Double valorFinal;
+
+    private TextView textViewValorPresente;
+    private EditText editTextTaxaDeJuros;
+    private EditText editTextPeriodos;
+    private TextView textViewResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compostos);
+        textViewValorPresente = findViewById(R.id.textViewValorPresenteComposto);
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getDouble("value");
+        textViewValorPresente.setText("Valor : $" +valorPresente.toString());
+
+        editTextTaxaDeJuros = findViewById(R.id.editTaxaJurosComposto);
+        editTextPeriodos = findViewById(R.id.editPeriodoComposto);
+        textViewResultado = findViewById(R.id.textViewResultadoComposto);
+    }
+
+
+    public void retornarClick(View view){
+        Intent resultado = new Intent();
+        resultado.putExtra("resultado", valorFinal);
+        setResult(MainActivity.RESULT_COMPOSTO, resultado);
+        finish();
+    }
+
+
+    public void calcularClick(View view ){
+        Double taxaDeJuros;
+        Integer periodos;
+        taxaDeJuros = Double.parseDouble(editTextTaxaDeJuros.getText().toString())/100.0;
+        periodos = Integer.parseInt(editTextPeriodos.getText().toString());
+        valorFinal = valorPresente*Math.pow(1+taxaDeJuros,periodos);
+        textViewResultado.setText("Resultado: $" +valorFinal.toString());
     }
 }
